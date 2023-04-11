e9b181f438f424b60bd16ec994099a009533851c
jeefo <jeefo@rwsh.ru>
2023-04-12 00:26:06 +0300

    bot: only stop for reloading in normal task

--- src/botlib.cpp
+++ src/botlib.cpp
@@ -1599,7 +1599,7 @@ void Bot::overrideConditions () {
    }
 
    // special handling for reloading
-   if (m_reloadState != Reload::None && m_isReloading && !isInNarrowPlace ()) {
+   if (getCurrentTaskId () == Task::Normal && m_reloadState != Reload::None && m_isReloading && !isInNarrowPlace ()) {
       if (m_seeEnemyTime + 2.5f < game.time () && (m_states & (Sense::SuspectEnemy | Sense::HearingEnemy))) {
          m_moveSpeed = m_fearLevel > m_agressionLevel ? 0.0f : getShiftSpeed ();
          m_navTimeset = game.time ();
