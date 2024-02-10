e7a67347f13fd799e62b746f2fdb2dfe056c2c87
jeefo <jeefo@rwsh.ru>
2024-02-10 22:50:53 +0300

    graph: enable graph c collection by default
    bot: bump version to 4.5

--- inc/version.h
+++ inc/version.h
@@ -13,6 +13,6 @@
 #define MODULE_AUTHOR "default@mail.net"
 #define MODULE_MACHINE "localhost"
 #define MODULE_COMPILER "default"
-#define MODULE_VERSION "4.0"
-#define MODULE_VERSION_FILE 4,0,0,000
+#define MODULE_VERSION "4.5"
+#define MODULE_VERSION_FILE 4,5,0,000
 #define MODULE_BUILD_ID "0:0"
