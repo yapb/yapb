a994e4c00164e5fd61281c849481c01ef12e755e
jeefo <dmitry@jeefo.net>
2026-02-27 22:08:13 +0300

    cfg: update yapb.cfg to reflect cvar name changes
    cvar: use new (csbot) smoke checks

--- src/support.cpp
+++ src/support.cpp
@@ -11,7 +11,7 @@ ConVar cv_display_welcome_text ("display_welcome_text", "1", "Enables or disable
 ConVar cv_enable_query_hook ("enable_query_hook", "0", "Enables or disables fake server query responses, which show bots as real players in the server browser.");
 ConVar cv_enable_fake_steamids ("enable_fake_steamids", "0", "Allows or disallows bots to return a fake Steam ID.");
 
-ConVar cv_smoke_grenade_checks ("smoke_grenade_checks", "1", "Affects the bot's vision by smoke clouds.", true, 0.0f, 2.0f);
+ConVar cv_smoke_grenade_checks ("smoke_grenade_checks", "2", "Affects the bot's vision by smoke clouds.", true, 0.0f, 2.0f);
 ConVar cv_smoke_grenade_radius ("smoke_grenade_radius", "240", "Radius to check for smoke clouds around a detonated grenade.", true, 32.0f, 320.0f);
 
 BotSupport::BotSupport () {
@@ -404,7 +404,7 @@ bool BotSupport::isLineBlockedBySmoke (const Vector &from, const Vector &to) {
          continue;
       }
 
-      const float smokeRadiusSq = cr::sqrf (smoke_grenade_radius.as <float> ());
+      const float smokeRadiusSq = cr::sqrf (cv_smoke_grenade_radius.as <float> ());
       const auto &smokeOrigin = sgtrack.find (pent);
 
       Vector toGrenade = smokeOrigin - from;
@@ -481,7 +481,7 @@ bool BotSupport::isLineBlockedBySmoke (const Vector &from, const Vector &to) {
    }
 
    // define how much smoke a bot can see thru
-   const float maxSmokedLength = 0.7f * smoke_grenade_radius.as <float> ();
+   const float maxSmokedLength = 0.7f * cv_smoke_grenade_radius.as <float> ();
 
    // return true if the total length of smoke-covered line-of-sight is too much
    return totalSmokedLength > maxSmokedLength;
